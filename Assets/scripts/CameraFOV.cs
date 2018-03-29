57f26d82dd9bcdb42cc433c6846134e9b7dddbd6
Tuomo Heikkinen <tuomoheikkinen3@kamk.fi>
2018-03-29 15:32:59 +0300

    SecurityCamera lisätty
    ...

---
+++ Assets/scripts/CameraFOV.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraFOV : MonoBehaviour
+{
+    public bool isSeen;
+    public float borderViewNegative = -23f;
+    public float borderViewPositive = 23f;
+    public float viewLength = 3;
+
+    void FixedUpdate()
+    {
+
+        RaycastHit hit;
+        RaycastHit hit2;
+        RaycastHit hit3;
+
+        //Vector3 forward = transform.TransformDirection(Vector3.forward) * 3;
+        Quaternion spreadAngleNegative = Quaternion.AngleAxis(borderViewNegative, Vector3.up);
+        Quaternion spreadAnglePositive = Quaternion.AngleAxis(borderViewPositive, Vector3.up);
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, viewLength))
+        {
+            if (hit.collider.tag == "Player")
+            {
+                ReloadScene();
+            }
+        }
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(spreadAngleNegative * Vector3.forward), out hit2, viewLength))
+        {
+            if (hit2.collider.tag == "Player")
+            {
+                ReloadScene();
+            }
+        }
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(spreadAnglePositive * Vector3.forward), out hit3, viewLength))
+        {
+            if (hit3.collider.tag == "Player")
+            {
+                ReloadScene();
+            }
+        }
+    }
+
+    public static void ReloadScene()
+    {
+        string currentScene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadSceneAsync(currentScene);
+    }
+}
