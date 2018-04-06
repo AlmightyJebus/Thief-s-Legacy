55c0e1ab9e5f8adbfe8cb1442d1df5326b7cf001
TaijaT <31925900+TaijaT@users.noreply.github.com>
2018-04-06 15:50:51 +0300

    warning texts

--- Assets/scripts/Patroller.cs
+++ Assets/scripts/Patroller.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Patroller : MonoBehaviour
 {
@@ -13,6 +14,23 @@ public class Patroller : MonoBehaviour
     public Transform pos6;
     public Transform pos7;
     public Transform pos8;
+    public Transform pos;
+
+
+    public bool position1 = true;
+    public bool position2 = false;
+    public bool position3 = false;
+    public bool position4 = false;
+    public bool position5 = false;
+    public bool position6 = false;
+    public bool position7 = false;
+    public bool position8 ;
+    
+
+
+
+    public GameObject questionText;
+    public GameObject warningText;
     
     public NavMeshAgent agent;
     EnemyFOV enemyScript;
@@ -20,6 +38,8 @@ public class Patroller : MonoBehaviour
     public Transform target;
     public Transform myTransform;
     public float followSpeed = 3f;
+    public bool start = true;
+
 
 
     void Start()
@@ -58,8 +78,30 @@ public class Patroller : MonoBehaviour
         {
             transform.LookAt(target);
             transform.Translate(Vector3.forward * followSpeed * Time.deltaTime);
+            warningText.SetActive(true);
         }
-        
+
+        if (enemyScript.isCautious == true && enemyScript.isDetected == false)
+        {
+            transform.Rotate(0, 50 * Time.deltaTime, 0);
+            questionText.SetActive(true);
+            start = false;
+        }
+        if (enemyScript.isCautious == false && start == false)
+        {
+            questionText.SetActive(false);
+            agent.SetDestination(pos1.position);
+            start = true;
+        }
+        if (enemyScript.isDetected == true && start == false)
+        {
+            
+            warningText.SetActive(true);
+        }
+        if (enemyScript.isDetected == false && start == true)
+        {
+            warningText.SetActive(false);
+        }
     }
 
 
@@ -70,15 +112,16 @@ public class Patroller : MonoBehaviour
       
        
 
-        if (enemyScript.isDetected == false)
+        if (enemyScript.isDetected == false && enemyScript.isCautious == false)
         {
 
-            agent.SetDestination(pos1.position);
+            
 
 
             if (other.tag == "1")
             {
                 agent.SetDestination(pos2.position);
+                start = true;
             }
 
             if (other.tag == "2")
@@ -117,10 +160,7 @@ public class Patroller : MonoBehaviour
             }
         }
 
-        if (enemyScript.isDetected == true)
-        {
-
-        }
+        
 
 
     }
