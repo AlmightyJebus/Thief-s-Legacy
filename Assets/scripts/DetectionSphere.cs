e083244815d096e01b02db46f07bfca7fb1f2f57
Tuomo Heikkinen <tuomoheikkinen3@kamk.fi>
2018-04-12 12:45:31 +0300

    DetectionSphere-script made

---
+++ Assets/scripts/DetectionSphere.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionSphere : MonoBehaviour {
+
+    public bool Detection = false;
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player") && PlayerMovement.pl.isSprinting)
+        {
+            Detection = true;
+        }
+
+        //:D
+    }
+}
