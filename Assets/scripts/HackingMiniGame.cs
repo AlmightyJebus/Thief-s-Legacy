cca39893186ebdb8c0d1a0950cdf7e5072db4657
Tuomo Heikkinen <tuomoheikkinen3@kamk.fi>
2018-04-18 10:28:33 +0300

    HackingMiniGame-script made

---
+++ Assets/scripts/HackingMiniGame.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGame : MonoBehaviour {
+
+    bool isSolved = false;
+
+    //1st sequence - Q F A R W D S E
+
+    private KeyCode[] sequenceFirst = new KeyCode[]{
+    KeyCode.Q,
+    KeyCode.F,
+    KeyCode.A,
+    KeyCode.R,
+    KeyCode.W,
+    KeyCode.D,
+    KeyCode.S,
+    KeyCode.E
+    };
+
+    private int sequenceIndex;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(sequenceFirst[sequenceIndex]))
+        {
+            if (++sequenceIndex == sequenceFirst.Length)
+            {
+                isSolved = true;
+            }
+        }
+
+        else if (Input.anyKeyDown)
+        {
+            isSolved = false; //?
+            sequenceIndex = 0;
+            EnemyFOV.efov.detectionPercent =+ 25f;
+        }
+    }
+}
