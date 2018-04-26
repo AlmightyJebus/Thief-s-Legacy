e02ab5b60076e410ee5f42f88c4b01bd8fda938a
Tuomo Heikkinen <tuomoheikkinen3@kamk.fi>
2018-04-26 16:03:50 +0300

    Door script

---
+++ Assets/scripts/DoorBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour
+{
+    public bool isOpen = false;
+    public GameObject doorLeft;
+    public GameObject doorRight;
+    public float openDistance = -3.0f;
+
+    private Vector3 openPosition;
+    private Vector3 closePosition;
+
+    void Start()
+    {
+        openPosition = doorLeft.transform.localPosition + new Vector3(0.0f, 0.0f, openDistance);
+        closePosition = doorLeft.transform.localPosition;
+
+        openPosition = doorRight.transform.localPosition - new Vector3(0.0f, 0.0f, openDistance);
+        closePosition = doorRight.transform.localPosition;
+    }
+
+    void Update()
+    {
+        if (isOpen == true)
+        {
+            
+        }
+    }
+}
