db2e50d80bddf76e4bddb26d353f78c03375a55f
Tuomo Heikkinen <tuomoheikkinen3@kamk.fi>
2018-05-03 11:14:14 +0300

    Depth-script

--- Assets/scripts/Depth.cs
+++ Assets/scripts/Depth.cs
@@ -6,6 +6,7 @@ public class Depth : MonoBehaviour
 {
     SpriteRenderer tempRend;
     float timer = 3;
+    public bool isPlayer;
 
     void Awake()
     {
@@ -15,5 +16,22 @@ public class Depth : MonoBehaviour
     void Update()
     {
         tempRend.sortingOrder = (int)Camera.main.WorldToScreenPoint(this.transform.position).y * -1;
+
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            tempRend.color = new Color(1, 1, 1, 0.5f);
+            timer = 3;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(isPlayer == false && other.GetComponent<Depth>().isPlayer == true)
+        {
+            tempRend.color = new Color(1, 1, 1, 0.5f);
+            timer = 3;
+        }
     }
 }
