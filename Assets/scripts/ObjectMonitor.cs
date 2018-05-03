ffc033497a192b6d6277f9b3dc51179f77e1aea0
Tuomo Heikkinen <tuomoheikkinen3@kamk.fi>
2018-05-03 13:34:12 +0300

    scripts

---
+++ Assets/scripts/ObjectMonitor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMonitor : MonoBehaviour
+{
+    public Collider playerAbove, playerBelow;
+    public static Transform player;
+    SpriteRenderer tempRend;
+    
+    void Awake()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+
+        tempRend = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(this.transform.position, player.position) < 20.0f)
+        {
+            decideColliderToUse();
+            decideSortingOrder();
+        }
+    }
+
+    void decideSortingOrder()
+    {
+        tempRend.sortingOrder = (int)Camera.main.WorldToScreenPoint(this.transform.position).y * -1;
+    }
+
+    void decideColliderToUse()
+    {
+        if (player.transform.position.y > this.transform.position.y)
+        {
+            playerAbove.enabled = true;
+            playerBelow.enabled = false;
+        }
+
+        else
+        {
+            playerAbove.enabled = false;
+            playerBelow.enabled = true;
+        }
+    }
+}
---
+++ Assets/scripts/PainterScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PainterScript : MonoBehaviour
+{
+
+    private List<GameObject> objects;
+    private List<SortObject> sortList;
+    public bool Sorting;
+
+    // Use this for initialization
+    void Start()
+    {
+        objects = GameObject.FindGameObjectsWithTag("Sortable").ToList();
+        sortList = new List<SortObject>();
+        foreach (GameObject item in objects)
+        {
+            sortList.Add(new SortObject(item.transform, item.GetComponent<SpriteRenderer>()));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Sorting)
+        {
+            //Sort positions list
+            sortList = sortList.OrderByDescending(v => v.Transform.position.y).ThenByDescending(v => v.Transform.position.x).ToList();
+
+            //for through and set order in layer
+            for (int i = 0; i < sortList.Count; i++)
+            {
+                sortList[i].SpriteRenderer.sortingOrder = i;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Struct to hold necessary data to sort objects in Unity.
+    /// </summary>
+    private struct SortObject
+    {
+        public Transform Transform;
+        public SpriteRenderer SpriteRenderer;
+
+        public SortObject(Transform transform, SpriteRenderer spriteRenderer)
+        {
+            Transform = transform;
+            SpriteRenderer = spriteRenderer;
+        }
+    }
+}
