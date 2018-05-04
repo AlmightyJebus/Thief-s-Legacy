e2a3c4ed90ee71d5dc0954e3db276134d7824e67
TaijaT <31925900+TaijaT@users.noreply.github.com>
2018-05-04 14:58:17 +0300

    cameralights

---
+++ Assets/rotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotator : MonoBehaviour {
+    public float objectVerticalAngle;
+    public float objectAngle;
+    public float maxRotation;
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.rotation = Quaternion.Euler(objectVerticalAngle, objectAngle + maxRotation * Mathf.Sin(Time.time * speed), 0f);
+    }
+}
