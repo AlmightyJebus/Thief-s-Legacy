2d98ecaa638f6ab5edacdb8b28d9eb696f894dfa
Tuomo Heikkinen <tuomoheikkinen3@kamk.fi>
2018-05-18 13:26:21 +0300

    menu scheisse

---
+++ Assets/scripts/MenuBackground.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBackground : MonoBehaviour
+{
+    public GameObject background;
+    public GameObject withTreasure;
+    public GameObject withoutTreasure;
+    public bool isStolen = false;
+    public float backgroundTimerVisible = 15f;
+    public float backgroundTimerNotVisible = 5f;
+    
+	void Update ()
+    {
+        backgroundTimerVisible = backgroundTimerVisible - Time.deltaTime;
+
+		if (isStolen == false)
+        {
+            background.SetActive(false);
+            withTreasure.SetActive(true);
+            withoutTreasure.SetActive(false);
+        }
+
+        if (isStolen == true)
+        {
+            withTreasure.SetActive(false);
+            background.SetActive(true);
+        }
+
+        if (backgroundTimerVisible <= 0)
+        {
+            isStolen = true;
+            backgroundTimerNotVisible = backgroundTimerNotVisible - Time.deltaTime;
+        }
+
+        if (backgroundTimerNotVisible <= 0)
+        {
+            background.SetActive(false);
+            withoutTreasure.SetActive(true);
+        }
+    }
+}
