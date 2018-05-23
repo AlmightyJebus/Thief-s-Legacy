72a82cbd1c274efe9d7fdf83bcab018ec52360c0
TaijaT <31925900+TaijaT@users.noreply.github.com>
2018-05-23 11:01:28 +0300

    lockpos script

---
+++ Assets/lockpos.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lockpos : MonoBehaviour
+{
+    public float lockPos;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, lockPos, lockPos);
+    }
+}
